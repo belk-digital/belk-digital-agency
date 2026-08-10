ee942d9417f44b628e5262d9b15c4c6295196f4f
Belk Digital <personalprojects@belkdigital.com>
2026-08-10 09:52:53 +0530

    fix: remove used captcha token from auto-reply send

--- src/screens/Contact.tsx
+++ src/screens/Contact.tsx
@@ -124,7 +124,7 @@ const Contact = () => {
           await emailjs.send(
             process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
             process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_USER_ID!,
-            { to_name: name, to_email: email, name, email, 'g-recaptcha-response': token },
+            { to_name: name, to_email: email, name, email },
             process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
           );
         }
